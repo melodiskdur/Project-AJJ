167eee83f9885753ca971446403f86f40ca61512
jonbah171 <jonbah171@studentmail.oru.se>
2021-06-13 20:30:07 +0200

    main.cpp : window creation

--- Project_AJJ/main.cpp
+++ Project_AJJ/main.cpp
@@ -1,7 +1,22 @@
 #include <stdlib.h>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Project AJJ");
+
+	sf::Event event;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+	}
 
 	return 0;
 }
