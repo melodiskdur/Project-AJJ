0c4a448563f3c3822b284bd4631ea1887d712f40
purpurbc <57225985+purpurbc@users.noreply.github.com>
2021-06-14 10:36:52 +0200

    added window.clear and display

--- Project_AJJ/main.cpp
+++ Project_AJJ/main.cpp
@@ -16,6 +16,15 @@ int main()
 				window.close();
 			}
 		}
+
+		// clear the window with black color
+		window.clear(sf::Color::Black);
+
+		// draw everything here...
+		// window.draw(...);
+
+		// end the current frame
+		window.display();
 	}
 
 	return 0;
