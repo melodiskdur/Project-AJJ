0facd429e4ca6c9f1ffb76fb917cb25765803d2e
melodiskdur <jonatanbartolini@gmail.com>
2022-02-05 10:54:07 +0100

    CloseCall-struct  added

--- Project_AJJ/includes/CollisionGraph.h
+++ Project_AJJ/includes/CollisionGraph.h
@@ -11,14 +11,29 @@
 * changes in position and velocity were to undo the
 * collision.
 */
-typedef struct _Collision_Edge
+typedef struct _CollisionEdge
 {
-	struct _Collision_Node* m_node;
-	struct _Collision_Node* m_adjacent;
+	struct _CollisionNode* m_node;
+	struct _CollisionNode* m_adjacent;
 	INTERSECTED_SIDE m_side;
 	sf::Vector2f proposed_repos;
 	sf::Vector2f proposed_revel;
-} Collision_Edge;
+} CollisionEdge;
+
+/* Represents an object in the absolute vicinity of an object involved
+* in a collision. That means any object whose distance to the collided
+* object is less than the size of the collided object (the largest distance
+  repositioning that we should expect). This is a way to keep the resolve
+  of an object to cause another collision.
+*/
+typedef struct _CloseCall
+{
+	Object* m_closecall_object;
+	struct _CollisionNode* m_node;
+	sf::Vector2f m_distance;
+	INTERSECTED_SIDE m_horizontal;
+	INTERSECTED_SIDE m_vertical;
+} CloseCall;
 
 /* Represents an object that has been involved in
 * at least one collision in a given frame. The
@@ -30,7 +45,7 @@ typedef struct _Collision_Edge
 * way to signal that the object has been moved as a result.
 * This is needed to recalibrate the remaining collisions (edges).
 */
-typedef struct _Collision_Node
+typedef struct _CollisionNode
 {
 	Object* m_node_object;
 	sf::Vector2f m_frame_pos;
@@ -38,9 +53,10 @@ typedef struct _Collision_Node
 	sf::Vector2f m_updated_pos;
 	sf::Vector2f m_updated_vel;
 	sf::Vector2f m_o_size;
-	std::vector<Collision_Edge> edges;
+	std::vector<CollisionEdge> m_outgoing_edges;
+	std::vector<CloseCall> m_close_calls;
 
-} Collision_Node;
+} CollisionNode;
 
 class CollisionGraph :
 	public PhysicsAttribute
