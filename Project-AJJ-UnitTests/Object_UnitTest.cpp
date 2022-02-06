c3b3934917e8d680298de38bf3c2190a3c51b2a9
melodiskdur <jonatanbartolini@gmail.com>
2022-02-06 16:04:27 +0100

    Added UnitTest-project to solution.

---
+++ Project-AJJ-UnitTests/Object_UnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "Object.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ProjectAJJUnitTests
+{
+	TEST_CLASS(ProjectAJJUnitTests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
