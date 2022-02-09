c740d30a3ed9413c6a90ade466f44787c1ca82af
melodiskdur <jonatanbartolini@gmail.com>
2022-02-09 10:31:21 +0100

    CollisionGraph storing-functionality added. The resolving-part is under way.

--- Project_AJJ/includes/CollisionGraph.h
+++ Project_AJJ/includes/CollisionGraph.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "PhysicsAttribute.h"
+#include "CollisionDetection.h"
+#include "Hitbox.h"
 #include "Object.h"
 #include <math.h>
+
+#define REPOS_INFINITY 200000.f
 
 /* Represents a collision between two objects. The struct
 * in itself is a directed edge between Collisios_Nodes,
@@ -16,24 +20,12 @@ typedef struct _CollisionEdge
 	struct _CollisionNode* m_node;
 	struct _CollisionNode* m_adjacent;
 	INTERSECTED_SIDE m_side;
-	sf::Vector2f proposed_repos;
-	sf::Vector2f proposed_revel;
+	sf::Vector2f m_proposed_resolve = { REPOS_INFINITY, REPOS_INFINITY };
+	std::vector<_CollisionEdge*> m_bonus_resolves;
+	bool m_is_relaxed = false;
+	bool m_is_visited = false;
+	int m_storage_index;
 } CollisionEdge;
-
-/* Represents an object in the absolute vicinity of an object involved
-* in a collision. That means any object whose distance to the collided
-* object is less than the size of the collided object (the largest distance
-  repositioning that we should expect). This is a way to keep the resolve
-  of an object to cause another collision.
-*/
-typedef struct _CloseCall
-{
-	Object* m_closecall_object;
-	struct _CollisionNode* m_node;
-	sf::Vector2f m_distance;
-	INTERSECTED_SIDE m_horizontal;
-	INTERSECTED_SIDE m_vertical;
-} CloseCall;
 
 /* Represents an object that has been involved in
 * at least one collision in a given frame. The
@@ -49,21 +41,65 @@ typedef struct _CollisionNode
 {
 	Object* m_node_object;
 	sf::Vector2f m_frame_pos;
-	sf::Vector2f m_frame_vel;
 	sf::Vector2f m_updated_pos;
-	sf::Vector2f m_updated_vel;
-	sf::Vector2f m_o_size;
-	std::vector<CollisionEdge> m_outgoing_edges;
-	std::vector<CloseCall> m_close_calls;
-
+	std::vector<CollisionEdge*> m_primary;
+	std::vector<CollisionEdge*> m_relaxed;
+	std::vector<CollisionEdge*> m_triggered;
+	std::vector<CollisionEdge*> m_unresolved;
+	int m_storage_index;
 } CollisionNode;
 
+/*
+* INFO HERE
+*/
 class CollisionGraph :
 	public PhysicsAttribute
 {
 public:
 	CollisionGraph();
 	~CollisionGraph();
+
+	// Reads data from a vector of collision tuples and creates
+	// nodes and edges accordingly. Note that resolves are calculating as well.
+	void storeCollisions(std::vector<ObjectTuple> collision_tuples);
+
+	// Looks for the CloseCallHolder.m_object node and stores the close calls
+	// as relaxed edges.
+	void storeCloseCalls(std::vector<CloseCallHolder>& close_calls);
+
+	// Creates and stores a CollisionNode for odata.m_object. Returns
+	// the index of the node in vector node_storage.
+	int createNode(ObjectData& odata);
+	int createNode(Object* object);
+
+	// Creates and stores a CollisionEdge for odata.m_object.
+	void createEdge(ObjectData& odata);
+	void createEdge(ObjectData& odata, CollisionNode* node, CollisionNode* adjacent);
+	// Adds a collision close call to 'node' by creating a CollisionEdge and setting it as relaxed. 
+	void createCloseCallEdge(CollisionNode* node, CollisionNode* adjacent);
+
 private:
+	// Node storage vector + pointer vectors.
+	std::vector<CollisionNode> node_storage;
+	std::vector<CollisionNode*> primary_collisions;
+	std::vector<CollisionNode*> triggered_collisions;
+	std::vector<CollisionNode*> relaxed_collisions;
+	std::vector<CollisionNode*> unresolved_collisions;
+
+	// Edge storage vector + pointer vectors.
+	std::vector<CollisionEdge> edge_storage;
+	std::vector<CollisionNode*> active_edges;
+
+	// Used internally to check if an object already has a CollisonNode.
+	// Returns -1 if the node doesn't exist.
+	int indexOf(Object* o);
+
+	// Used internally to sort the primary edges in number
+	// of resolves that are created (one edge.proposed
+	// could for example solve 2 other collisions).
+	void sortEdges(CollisionNode* c);
+
+	// Used internally to reset all the vectors.
+	void clearVectors();
 };
 
--- Project_AJJ/src/CollisionGraph.cpp
+++ Project_AJJ/src/CollisionGraph.cpp
@@ -10,3 +10,141 @@ CollisionGraph::~CollisionGraph()
 {
 
 }
+
+void CollisionGraph::storeCollisions(std::vector<ObjectTuple> collision_tuples)
+{
+	// Clear vectors before storing. Refactored for readability.
+	this->clearVectors();
+	for (auto& tuple: collision_tuples)
+	{
+		// Hitbox resolve calculations before creating and storing.
+		std::vector<ObjectData> v_odata = Hitbox::separateHitboxes(tuple.obj_i, tuple.obj_j);
+		// Creating and storing.
+		for (auto& odata : v_odata)
+		{
+			int i_odata = indexOf(odata.m_object);
+			int i_odata_collided = indexOf(odata.m_colliding_object);
+			// Create CollisionNode for m_object if it doesn't already exist.
+			if (i_odata == -1) i_odata = this->createNode(odata);
+			//Create CollisionNode for m_colliding_object as well if necessary.
+			if (i_odata_collided == -1) i_odata_collided = this->createNode(odata.m_colliding_object);
+			// Create and store CollisionEdge.
+			this->createEdge(odata, &this->node_storage[i_odata], &this->node_storage[i_odata_collided]);
+		}
+	}
+}
+
+void CollisionGraph::storeCloseCalls(std::vector<CloseCallHolder>& close_calls)
+{
+	for (auto& holder : close_calls)
+	{
+		int i_holder_object = indexOf(holder.m_object);
+		// Create CollisionNode for m_object if it doesn't exist.
+		if (i_holder_object == -1) i_holder_object = this->createNode(holder.m_object);
+		// Store close call-edges.
+		for (auto& close_call : holder.m_close_calls)
+		{
+			int i_closecall_object = indexOf(close_call);
+			// Create CollisionNode for close_call if it doesn't exist.
+			if (i_closecall_object == -1) i_closecall_object = this->createNode(close_call);
+			// Create close call edge.
+			this->createCloseCallEdge(&this->node_storage[i_holder_object], &this->node_storage[i_closecall_object]);
+		}
+	}
+}
+
+int CollisionGraph::createNode(ObjectData& odata)
+{
+	CollisionNode new_node;
+	new_node.m_node_object = odata.m_object;
+	new_node.m_frame_pos = odata.m_object->getWorldPosition();
+	new_node.m_updated_pos = new_node.m_frame_pos;
+	new_node.m_storage_index = this->node_storage.size();
+
+	this->node_storage.push_back(new_node);
+
+	// Store pointer in G.primaryCollisions.
+	CollisionNode* node_ptr = &this->node_storage[new_node.m_storage_index];
+	this->primary_collisions.push_back(node_ptr);
+
+	return new_node.m_storage_index;
+}
+
+int CollisionGraph::createNode(Object* object)
+{
+	CollisionNode new_node;
+	new_node.m_node_object = object;
+	new_node.m_frame_pos = object->getWorldPosition();
+	new_node.m_updated_pos = new_node.m_frame_pos;
+	new_node.m_storage_index = this->node_storage.size();
+
+	// Store pointer in G.primaryCollisions.
+	CollisionNode* node_ptr = &this->node_storage[new_node.m_storage_index];
+	this->primary_collisions.push_back(node_ptr);
+
+	this->node_storage.push_back(new_node);
+	return new_node.m_storage_index;
+}
+
+void CollisionGraph::createEdge(ObjectData& odata)
+{
+	int i_node = this->indexOf(odata.m_object);
+	int i_colliding = this->indexOf(odata.m_colliding_object);
+	// Exit function if at least one of the CollisionNodes is NOT found.
+	if (i_node == -1 || i_colliding == -1) return;
+	// Else call the other createEdge-function where the functionality is implemented.
+	this->createEdge(odata, &this->node_storage[i_node], &this->node_storage[i_colliding]);
+}
+
+void CollisionGraph::createEdge(ObjectData& odata, CollisionNode* node, CollisionNode* adjacent)
+{
+	CollisionEdge new_edge;
+	new_edge.m_node = node;
+	new_edge.m_adjacent = adjacent;
+	new_edge.m_proposed_resolve = odata.m_wp;
+	new_edge.m_side = odata.m_intersect;
+	new_edge.m_storage_index = this->edge_storage.size();
+
+	this->edge_storage.push_back(new_edge);
+
+	// Store pointer to the CollisionEdge in m_primary.
+	CollisionEdge* edge_ptr = &this->edge_storage[new_edge.m_storage_index];
+	node->m_primary.push_back(edge_ptr);
+}
+
+void CollisionGraph::createCloseCallEdge(CollisionNode* node, CollisionNode* adjacent)
+{
+	CollisionEdge new_edge;
+	new_edge.m_node = node;
+	new_edge.m_adjacent = adjacent;
+	// Set the edge as relaxed.
+	new_edge.m_is_relaxed = true;
+
+	// Store pointer to edge in m_relaxed.
+	CollisionEdge* edge_ptr = &this->edge_storage[new_edge.m_storage_index];
+	node->m_relaxed.push_back(edge_ptr);
+}
+
+int CollisionGraph::indexOf(Object* o)
+{
+	for (int i = 0; i < this->node_storage.size(); i++)
+		// Return index if equivalent object is found.
+		if (this->node_storage[i].m_node_object == o) return i;
+	// Return -1 if no corresponding CollisionNode is found.
+	return -1;
+}
+
+void CollisionGraph::sortEdges(CollisionNode* c)
+{
+}
+
+void CollisionGraph::clearVectors()
+{
+	this->node_storage.clear();
+	this->primary_collisions.clear();
+	this->triggered_collisions.clear();
+	this->relaxed_collisions.clear();
+	this->unresolved_collisions.clear();
+	this->edge_storage.clear();
+	this->active_edges.clear();
+}
